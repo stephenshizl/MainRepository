5d2bf123d1747a614a851c86d5adad65c5df6802
latou19921203 <latou.main@foxmail.com>
2017-06-22 17:29:10 +0800

    2017年6月22日17:27:32
    Update Login Demo:
    Save User Info in File or Preference.

---
+++ Android_Demos/login/activity/NextPageActivity.java
@@ -0,0 +1,36 @@
+package com.example.a61555.sharedpreferencedemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Map;
+
+/**
+ * Created by 61555 on 2017/6/22.
+ */
+
+public class NextPageActivity extends AppCompatActivity {
+
+    private TextView usernameTextView;
+    private TextView passwordTextView;
+    private SaveInfoUtils saveInfoUtils;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.next_page);
+        init();
+    }
+
+    public void init(){
+        saveInfoUtils = new SaveInfoUtils(this);
+        //Map infoMap = saveInfoUtils.getUserInfoFromFile();
+        Map infoMap = saveInfoUtils.getUserInfoFromPref();
+
+        usernameTextView = (TextView) findViewById(R.id.username_show);
+        passwordTextView = (TextView) findViewById(R.id.password_show);
+
+        usernameTextView.setText((String) infoMap.get("username"));
+        passwordTextView.setText((String) infoMap.get("password"));
+    }
+}
