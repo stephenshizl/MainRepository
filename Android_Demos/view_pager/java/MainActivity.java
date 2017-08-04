5a733d5c76dff52db8393eca9f9b60fbe79ea6c9
latou19921203 <latou.main@foxmail.com>
2017-08-04 15:33:51 +0800

    2017年8月4日15:33:45
    Update

--- Android_Demos/view_pager/java/MainActivity.java
+++ Android_Demos/view_pager/java/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
-import android.widget.Toast;
 
 /**
  * 使用FragmentPagerStateAdapter/FragmentPagerAdapter & Fragment & ViewPager
@@ -47,19 +46,44 @@ public class MainActivity extends FragmentActivity{
         viewPager.setCurrentItem(FIRST_VIEW_PAGER_NUM);
         //添加页面切换的监听器
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            /**
+             * 当页面在滑动时至滑动停止之前，此方法会一直调用
+             * @param position 页面编号，向左滑动时为起始页面编号和终止页面编号（从起始变到终止）；向右滑动时为终止页面编号
+             * @param positionOffset 当前页面偏移百分比
+             * @param positionOffsetPixels 当前页面偏移的像素位置
+             */
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
             }
 
+            /**
+             * 页面跳转完后调用
+             * @param position 当前所在页编号
+             */
             @Override
             public void onPageSelected(int position) {
-                Toast.makeText(getApplicationContext(), "PAGE "+(position+1)+" BE SELECTED", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), "PAGE "+(position+1)+" BE SELECTED", Toast.LENGTH_SHORT).show();
             }
 
+            /**
+             * 页面状态改变时调用，页面状态分为静止、滑动时和滑动后
+             * @param state 页面状态
+             */
             @Override
             public void onPageScrollStateChanged(int state) {
 
+                switch (state) {
+                    case ViewPager.SCROLL_STATE_DRAGGING://滑动状态
+
+                        break;
+                    case ViewPager.SCROLL_STATE_IDLE://空闲状态
+
+                        break;
+                    case ViewPager.SCROLL_STATE_SETTLING://滑动后自然沉降的状态
+
+                        break;
+                }
             }
         });
     }
